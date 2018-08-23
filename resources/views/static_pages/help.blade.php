39983f5a91cce31829256ad7c287c9c55d75a18b
lynn <lin438056372@163.com>
2018-08-23 15:48:03 +0000

    fix help view

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>vlynn.gg - Laravel Primer</title>
-</head>
-<body>
+@extends('layout.default')
+@section('content')
 	<h1>帮助页</h1>
-</body>
-</html>
+@stop
