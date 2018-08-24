f505fb71d8fc3e35570326fa7d04bee489a72724
lynn <lin438056372@163.com>
2018-08-24 17:12:29 +0000

    fix

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,6 @@ return [
     */
 
     'connections' => [
-        'default' => $db_config['connection'],
 
         'sqlite' => [
             'driver' => 'sqlite',
